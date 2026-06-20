84b16a0bdb558e692daa30391c3194c4072ce918
AlanAI01 <allan.kairke@bk.ru>
2026-06-20 19:14:42 +0600

    Added Flask server for Render

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
+from flask import Flask
+import threading
+import os
 import asyncio
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
-
+import os
 TOKEN = "8859657712:AAHvfQWEv5V3ztH7ZFv9nL1bep-4KAJ9q7g"
 
 bot = Bot(token=TOKEN)
@@ -34,6 +37,16 @@ async def answer_message(message: Message):
 async def main():
     await dp.start_polling(bot)
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is alive!"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
+    threading.Thread(target=run_web).start()
     asyncio.run(main())
